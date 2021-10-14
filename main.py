21a8850c424227d2ced955dd9c1a9911b4868f95
Creighton France <tkdg25@gmail.com>
2021-10-13 23:17:27 -0500

    Sorted data file output

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 
 # Open file to write raw data
 rawDataFile = open('raw3daydata.txt', 'w')
+sortedData = open('3daydata.txt', 'w')
 
 # Pull information from the site
 resp = requests.get('https://sheriff.washingtoncountyar.gov/res/DIntakeRoster.aspx')
@@ -13,8 +14,9 @@ soup = BeautifulSoup(resp.text, 'html.parser')
 
 # Write a pretty copy to the raw data file
 #rawDataFile.write(soup.body.div.table.prettify())
-for i, element in enumerate(soup.body.div.table.find_all("tr")):
+for i, element in enumerate(soup.body.div.table.find_all('tr')):
     if (i != 0):
+        sortedData.write("---------------\n")
         print("\n" + str(i))
         print(element.contents[1].a['href'])
         print(element.contents[1].get_text())
@@ -25,7 +27,17 @@ for i, element in enumerate(soup.body.div.table.find_all("tr")):
         print(element.contents[11].get_text())
         print(element.contents[13].get_text())
         print(element.contents[15].get_text())
+        sortedData.write(element.contents[1].get_text() + "\n")
+        sortedData.write(element.contents[3].get_text() + "\n")
+        sortedData.write(element.contents[5].get_text() + "\n")
+        sortedData.write(element.contents[7].get_text() + "\n")
+        sortedData.write(element.contents[9].get_text() + "\n")
+        sortedData.write(element.contents[11].get_text() + "\n")
+        sortedData.write(element.contents[13].get_text() + "\n")
+        sortedData.write(element.contents[15].get_text() + "\n")
 
-# Close raw data file
 rawDataFile.write(soup.prettify())
+
+# Close data files
 rawDataFile.close()
+sortedData.close()
