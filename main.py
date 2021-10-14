84ca283e17cf2badc0e9d3bc2cc352f10dd204c6
Creighton France <tkdg25@gmail.com>
2021-10-13 21:42:23 -0500

    prettify

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 
 rawDataFile = open('raw3daydata.txt', 'w')
 
 resp = requests.get('https://sheriff.washingtoncountyar.gov/res/DIntakeRoster.aspx')
+soup = BeautifulSoup(resp.text, 'html.parser')
 
-rawDataFile.write(resp.text)
-
+rawDataFile.write(soup.prettify())
 rawDataFile.close()
